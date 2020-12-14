20c7b336b8e9336d20114411c3a968d4a479fac9
devenock <enockomondi305@gmail.com>
2020-12-14 14:57:09 +0300

    working on the register form

---
+++ register/register.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+const messageContainer = document.getElementById('message');
+
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+
+form.addEventListener('submit', (e) =>{
+    
+})
